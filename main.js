5976e90e2ef8abea032a104c514d12fa90604f7d
maximino13 <victormaxter13@gmail.com>
2023-09-05 06:18:56 -0500

    Se agrego el Aside de Carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-const menuEmail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email'); 
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+function toggleDesktopMenu() {  
+    desktopMenu.classList.toggle('inactive');  //a través del metodo Toggle se accede a la Lista para Activarla (Si se encuentra inactiva) y vicersa
+    aside.classList.add('inactive');// agregamos con Add el método innactivo 
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+    mobileMenu.classList.toggle('inactive');  //a través del metodo Toggle se accede a la Lista para Activarla (Si se encuentra inactiva) y vicersa
+    aside.classList.add('inactive'); // agregamos con Add el método innactivo 
+}
+
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive'); //a través del metodo Toggle se accede a la Lista para Activarla (Si se encuentra inactiva) y vicersa
+    mobileMenu.classList.add('inactive'); // agregamos con Add el método innactivo 
+    desktopMenu.classList.add('inactive'); // agregamos con Add el método innactivo 
 }
